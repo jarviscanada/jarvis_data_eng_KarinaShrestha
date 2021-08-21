da8f3b496acf48ebd2e1e2c9bd33b70a461bb6ef
Karina Shrestha <karinashrestha.ks@gmail.com>
2021-08-21 19:52:04 +0000

    dao & service implementation

---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/OddEvenTest.java
@@ -0,0 +1,21 @@
+package ca.jrvs.practice.codingChallenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OddEvenTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void oddEvenMod() {
+  }
+
+  @Test
+  public void oddEvenBit() {
+  }
+}
